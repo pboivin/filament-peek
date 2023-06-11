69d7c8027f1417a71769b3eb55f61cf0e63109c6
Patrick Boivin <pboivin@gmail.com>
2023-06-11 09:03:10 -0400

    Version dist assets

--- src/FilamentPeekServiceProvider.php
+++ src/FilamentPeekServiceProvider.php
@@ -9,14 +9,16 @@ use Spatie\LaravelPackageTools\Package;
 
 class FilamentPeekServiceProvider extends PluginServiceProvider
 {
+    const VERSION = '0.3.0';
+
     public static string $name = 'filament-peek';
 
     protected array $styles = [
-        'plugin-filament-peek' => __DIR__.'/../resources/dist/filament-peek.css',
+        'plugin-filament-peek-' . self::VERSION => __DIR__ . '/../resources/dist/filament-peek.css',
     ];
 
-    protected array $scripts = [
-        'plugin-filament-peek' => __DIR__.'/../resources/dist/filament-peek.js',
+    protected array $beforeCoreScripts = [
+        'plugin-filament-peek-' . self::VERSION => __DIR__ . '/../resources/dist/filament-peek.js',
     ];
 
     public function configurePackage(Package $package): void
