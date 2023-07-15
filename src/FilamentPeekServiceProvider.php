20ab148e06eacdd4b4d559d000ee9a085384d63d
Patrick Boivin <pboivin@gmail.com>
2023-07-15 13:20:27 -0400

    Bump version

--- src/FilamentPeekServiceProvider.php
+++ src/FilamentPeekServiceProvider.php
@@ -11,7 +11,7 @@ use Spatie\LaravelPackageTools\Package;
 
 class FilamentPeekServiceProvider extends PluginServiceProvider
 {
-    const VERSION = '1.0.0-beta1';
+    const VERSION = '1.0.0-beta2';
 
     public static string $name = 'filament-peek';
 
