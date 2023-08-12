11f6f64d903fd09ccc14d78dd69349a25b482263
Patrick Boivin <pboivin@gmail.com>
2023-08-12 12:01:07 -0400

    Lint

--- src/Forms/Actions/InlinePreviewAction.php
+++ src/Forms/Actions/InlinePreviewAction.php
@@ -11,7 +11,7 @@ class InlinePreviewAction extends Action
 
     public static function getDefaultName(): ?string
     {
-        return 'inlinePreview' . static::$count++;
+        return 'inlinePreview'.static::$count++;
     }
 
     protected function setUp(): void
