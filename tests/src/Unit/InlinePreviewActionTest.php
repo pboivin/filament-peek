dec98f977f0fd535036f0d93ebc5feb60fa087c4
Patrick Boivin <pboivin@gmail.com>
2023-08-12 12:43:36 -0400

    test: InlinePreviewAction

---
+++ tests/src/Unit/InlinePreviewActionTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Pboivin\FilamentPeek\Tests\Unit;
+
+use Filament\Pages\Page;
+use Illuminate\Support\Facades\View;
+use Pboivin\FilamentPeek\Forms\Actions\InlinePreviewAction;
+use Pboivin\FilamentPeek\Support;
+
+it('has a default name', function () {
+    $previewAction = InlinePreviewAction::make();
+
+    expect($previewAction->getDefaultName())->toHavePrefix('inlinePreview');
+});
+
+it('has a default label', function () {
+    $previewAction = InlinePreviewAction::make();
+
+    expect($previewAction->getLabel())->toBe('Preview');
+});
+
+it('has a default action', function () {
+    $previewAction = InlinePreviewAction::make()
+        ->livewire($this->mock(Page::class));
+
+    expect(is_callable($previewAction->getActionFunction()))->toBeTrue();
+});
+
+it('sets the view hook to render the modal', function () {
+    InlinePreviewAction::make();
+
+    $shared = View::getShared();
+
+    expect($shared[Support\View::PREVIEW_ACTION_SETUP_HOOK])->toBeTrue();
+    expect(isset($shared[Support\View::BUILDER_PREVIEW_SETUP_HOOK]))->toBeFalse();
+});
+
+it('sets the view hook to render a builder preview', function () {
+    InlinePreviewAction::make()
+        ->builderPreview('test');
+
+    $shared = View::getShared();
+
+    expect($shared[Support\View::PREVIEW_ACTION_SETUP_HOOK])->toBeTrue();
+    expect($shared[Support\View::BUILDER_PREVIEW_SETUP_HOOK])->toBeTrue();
+});
