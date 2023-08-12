9be3549547e43c7d6136574baac4a47986914451
Patrick Boivin <pboivin@gmail.com>
2023-08-12 11:59:00 -0400

    feat: Add InlinePreviewAction

---
+++ src/Forms/Actions/InlinePreviewAction.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Pboivin\FilamentPeek\Forms\Actions;
+
+use Filament\Forms\Components\Actions\Action;
+use Pboivin\FilamentPeek\Support;
+
+class InlinePreviewAction extends Action
+{
+    public static int $count = 1;
+
+    public static function getDefaultName(): ?string
+    {
+        return 'inlinePreview' . static::$count++;
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->label(__('filament-peek::ui.preview-action-label'))
+            ->link()
+            ->action(function ($livewire) {
+                Support\Panel::ensurePluginIsLoaded();
+
+                Support\Page::ensurePreviewModalSupport($livewire);
+
+                $arguments = $this->getArguments();
+
+                if ($builderField = $arguments['builderField'] ?? false) {
+                    Support\Page::ensureBuilderPreviewSupport($livewire);
+
+                    $livewire->openPreviewModalForBuidler($builderField);
+                } else {
+                    $livewire->openPreviewModal();
+                }
+            });
+
+        Support\View::setupPreviewModal();
+    }
+
+    public function builderPreview(string $builderField = 'blocks'): static
+    {
+        Support\View::setupBuilderEditor();
+
+        $this->arguments(['builderField' => $builderField]);
+
+        return $this;
+    }
+}
