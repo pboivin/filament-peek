379726122b0b0ef9be2e479fefe57751f35e1557
Patrick Boivin <pboivin@gmail.com>
2023-09-16 13:54:55 -0400

    Bump version

--- src/FilamentPeekPlugin.php
+++ src/FilamentPeekPlugin.php
@@ -16,7 +16,7 @@ class FilamentPeekPlugin implements Plugin
 
     const ID = 'filament-peek';
 
-    const VERSION = '2.0.0';
+    const VERSION = '2.0.1';
 
     protected bool $shouldLoadPluginScripts = true;
 
