118db03fce310d8134743919c7e9488490e5a327
Patrick Boivin <pboivin@gmail.com>
2023-09-16 11:04:58 -0400

    test: Add AssetsDisabledTest

---
+++ tests/src/Feature/AssetsDisabledTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Pboivin\FilamentPeek\Tests\Feature;
+
+use Pboivin\FilamentPeek\Tests\Models\User;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+
+it('does not see preview modal assets when disabled', function () {
+    actingAs(User::factory()->create());
+
+    get('/admin')
+        ->assertDontSee('filament-peek.css')
+        ->assertDontSee('filament-peek.js');
+});
