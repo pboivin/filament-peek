65f009789bf9f6e18eefc1a33b4403c9b9f01623
Patrick Boivin <pboivin@gmail.com>
2023-09-23 11:36:10 -0400

    test: ListPreviewAction

---
+++ tests/src/Unit/ListPreviewActionTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Pboivin\FilamentPeek\Tests\Unit;
+
+use Filament\Pages\Page;
+use Illuminate\Support\Facades\View;
+use Pboivin\FilamentPeek\Tables\Actions\ListPreviewAction;
+use Pboivin\FilamentPeek\Support;
+
+it('has a default name', function () {
+    $previewAction = ListPreviewAction::make();
+
+    expect($previewAction->getDefaultName())->toEqual('listPreview');
+});
+
+it('has a default label', function () {
+    $previewAction = ListPreviewAction::make();
+
+    expect($previewAction->getLabel())->toEqual('Preview');
+});
+
+it('has a default action', function () {
+    $previewAction = ListPreviewAction::make()
+        ->livewire($this->mock(Page::class));
+
+    expect(is_callable($previewAction->getActionFunction()))->toBeTrue();
+});
+
+it('sets the view hook to render the modal', function () {
+    ListPreviewAction::make();
+
+    $shared = View::getShared();
+
+    expect($shared[Support\View::PREVIEW_ACTION_SETUP_HOOK])->toBeTrue();
+});
