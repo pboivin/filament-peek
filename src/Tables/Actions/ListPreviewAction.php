5ce83b5066e965e6a87bc9000fcbe9c50c8967b9
Patrick Boivin <pboivin@gmail.com>
2023-09-23 11:36:02 -0400

    feat: Add ListPreviewAction

---
+++ src/Tables/Actions/ListPreviewAction.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Pboivin\FilamentPeek\Tables\Actions;
+
+use Filament\Tables\Actions\Action;
+use Pboivin\FilamentPeek\Support;
+
+class ListPreviewAction extends Action
+{
+    public static function getDefaultName(): ?string
+    {
+        return 'listPreview';
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->label(__('filament-peek::ui.preview-action-label'))
+            ->icon('heroicon-s-eye')
+            ->action(function ($livewire, $record) {
+                Support\Panel::ensurePluginIsLoaded();
+
+                Support\Page::ensurePreviewModalSupport($livewire);
+
+                $livewire->setPreviewableRecord($record);
+
+                $livewire->openPreviewModal();
+            });
+
+        Support\View::setupPreviewModal();
+    }
+}
