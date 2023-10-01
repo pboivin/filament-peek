9f137c320e0ed2c2b1a1f226316663fdb8af1b8e
Patrick Boivin <pboivin@gmail.com>
2023-10-01 15:55:38 -0400

    feat: Add internal preview url middleware config

--- config/filament-peek.php
+++ config/filament-peek.php
@@ -93,6 +93,8 @@ return [
 
     'useInternalPreviewUrl' => false,
 
+    'internalPreviewUrlMiddleWare' => [],
+
     /*
     |--------------------------------------------------------------------------
     | Builder Editor
