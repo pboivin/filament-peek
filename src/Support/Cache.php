d11eb21cfa6053c334772d4a19617ba4dd511064
Patrick Boivin <pboivin@gmail.com>
2023-10-10 10:06:18 -0400

    fix: Update createPreviewToken

--- src/Support/Cache.php
+++ src/Support/Cache.php
@@ -3,11 +3,12 @@
 namespace Pboivin\FilamentPeek\Support;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
 
 class Cache
 {
     public function createPreviewToken(): string
     {
-        return md5('preview-'.Auth::user()->getAuthIdentifier().(Auth::user()->password ?? ''));
+        return md5('preview-'.Auth::user()->getAuthIdentifier().Config::get('app.key', ''));
     }
 }
