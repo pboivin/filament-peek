42d5773b50a6dd8ab70cfab17f3c6b9b6868eebf
Jarka Pachlová <jarka.p@gmail.com>
2024-09-04 17:12:32 +0200

    Add czech translations

---
+++ resources/lang/cs/ui.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'preview-action-label' => 'Náhled',
+
+    'preview-modal-title' => 'Náhled',
+
+    'close-modal-action-label' => 'Zavřít',
+
+    'builder-editor-title' => 'Editor',
+
+    'refresh-action-label' => 'Obnovit náhled',
+
+    'editor-settings-label' => 'Nastavení',
+
+    'editor-auto-refresh-label' => 'Automatické obnovení',
+
+    'accept-action-label' => 'Přijmout',
+
+    'discard-action-label' => 'Zrušit',
+];
