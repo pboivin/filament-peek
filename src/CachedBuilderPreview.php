687a953e20dbd247793366610175c0d4c3190c52
Patrick Boivin <pboivin@gmail.com>
2025-04-08 12:43:00 -0400

    Cleanup

--- src/CachedBuilderPreview.php
+++ src/CachedBuilderPreview.php
@@ -4,14 +4,6 @@ namespace Pboivin\FilamentPeek;
 
 class CachedBuilderPreview extends CachedPreview
 {
-    public static function make(
-        string $pageClass,
-        string $view,
-        array $data,
-    ): CachedBuilderPreview {
-        return new CachedBuilderPreview($pageClass, $view, $data);
-    }
-
     public function render(): string
     {
         return $this->pageClass::renderBuilderPreview($this->view, $this->data);
