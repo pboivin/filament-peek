7ab16ebc8a1ac852e8fe6cc768753466f3bff362
Patrick Boivin <pboivin@gmail.com>
2025-07-10 11:06:42 -0400

    docs: Update links

--- generate-toc.php
+++ generate-toc.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Str;
 
 require_once './vendor/autoload.php';
 
-define('BASE_URL', 'https://github.com/pboivin/filament-peek/blob/2.x/');
+define('BASE_URL', 'https://github.com/pboivin/filament-peek/blob/3.x/');
 
 class DocFile
 {
