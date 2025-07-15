27bcaa372af08ee1f4b0e394e7fa4be536832ec4
Patrick Boivin <pboivin@gmail.com>
2025-07-15 11:51:27 -0400

    fix: Implement HasActions in BuilderEditor

--- src/Livewire/BuilderEditor.php
+++ src/Livewire/BuilderEditor.php
@@ -2,6 +2,8 @@
 
 namespace Pboivin\FilamentPeek\Livewire;
 
+use Filament\Actions\Concerns\InteractsWithActions;
+use Filament\Actions\Contracts\HasActions;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Illuminate\Contracts\View\View as ViewContract;
@@ -15,9 +17,10 @@ use Pboivin\FilamentPeek\Facades\Peek;
 /**
  * @property mixed $form
  */
-class BuilderEditor extends Component implements HasForms
+class BuilderEditor extends Component implements HasForms, HasActions
 {
     use InteractsWithForms;
+    use InteractsWithActions;
 
     public ?string $previewUrl = null;
 
