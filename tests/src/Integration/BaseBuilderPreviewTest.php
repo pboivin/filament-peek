291df57ed23b1128b54f1587257ff52f60660569
Patrick Boivin <pboivin@gmail.com>
2025-08-29 12:36:24 -0400

    Fix integration tests

---
+++ tests/src/Integration/BaseBuilderPreviewTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Pboivin\FilamentPeek\Tests\Integration;
+
+use Pboivin\FilamentPeek\Tests\App\Models\Category;
+use Pboivin\FilamentPeek\Tests\App\Models\Post;
+
+use function Pest\Laravel\get;
+
+it('sees builder editor when creating a post', function () {
+    $this->login();
+
+    get('/admin/posts/create')
+        ->assertSuccessful()
+        ->assertSee('Test_Builder_Preview')
+        ->assertSee('x-ref="builderEditor"', escape: false);
+});
+
+it('sees builder editor when editing a post', function () {
+    $this->login();
+
+    $post = Post::factory()
+        ->for(Category::factory(), 'category')
+        ->create();
+
+    get('/admin/posts/'.$post->id.'/edit')
+        ->assertSuccessful()
+        ->assertSee('Test_Builder_Preview')
+        ->assertSee('x-ref="builderEditor"', escape: false);
+});
---
+++ tests/src/Integration/BasePreviewModalTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Pboivin\FilamentPeek\Tests\Integration;
+
+use Pboivin\FilamentPeek\Tests\App\Models\Page;
+
+use function Pest\Laravel\get;
+
+it('sees preview modal assets by default', function () {
+    $this->login();
+
+    get('/admin')
+        ->assertSuccessful()
+        ->assertSee('filament-peek.css')
+        ->assertSee('filament-peek.js');
+});
+
+it('does not see preview modal on dashboard', function () {
+    $this->login();
+
+    get('/admin')
+        ->assertSuccessful()
+        ->assertDontSee('x-ref="previewModalBody"', escape: false);
+});
+
+it('sees preview modal when creating a page', function () {
+    $this->login();
+
+    get('/admin/pages/create')
+        ->assertSuccessful()
+        ->assertSee('Test_Preview_Action')
+        ->assertSee('x-ref="previewModalBody"', escape: false)
+        ->assertDontSee('x-ref="builderEditor"', escape: false);
+});
+
+it('sees preview modal when editing a page', function () {
+    $this->login();
+
+    $page = Page::factory()->create();
+
+    get('/admin/pages/'.$page->id.'/edit')
+        ->assertSuccessful()
+        ->assertSee('Test_Preview_Action')
+        ->assertSee('x-ref="previewModalBody"', escape: false)
+        ->assertDontSee('x-ref="builderEditor"', escape: false);
+});
