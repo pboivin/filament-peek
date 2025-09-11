629aaffe0fec8cfaacf38041cc2ea262ceef9b62
Patrick Boivin <pboivin@gmail.com>
2025-09-11 09:26:44 -0400

    Cleanup [skip ci]

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-const preset = require('./vendor/filament/support/tailwind.config.preset')
-
-module.exports = {
-    presets: [preset],
-    content: [
-        './resources/views/**/*.blade.php',
-        './src/**/*.php',
-    ],
-}
